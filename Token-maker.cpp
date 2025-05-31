6d7a96a9533c59834a22fc1acb01025cf5078b01
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2025-06-01 02:18:20 +0300

    Token-maker.cpp

--- Token-maker.cpp
+++ Token-maker.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+vector <string> tokenisator(const string& str) {
+    vector <string> list;
+    string word;
+    for (int i = 0; i < str.size(); ++i) {
+        char ch = str[i];
+        ch = tolower(ch);
+        if (isalnum(ch)) {
+            word += ch;
+        } else {
+            if (!word.empty()) {
+                list.push_back(word);
+                word.clear();
+            }
+        }
+    }
+
+    if (!word.empty()) {
+        list.push_back(word);
+        word.clear();
+    }
+
+    return list;
+}
 
