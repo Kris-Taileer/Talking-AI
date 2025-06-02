4edbd77a878bab48d82238abc0e089074eecb0c9
Kris Taileer <191796700+Kris-Taileer@users.noreply.github.com>
2025-06-02 09:16:39 +0300

    Update RESPONSE-ibility.cpp

--- RESPONSE-ibility.cpp
+++ RESPONSE-ibility.cpp
@@ -10,10 +10,10 @@ string res_gen(sqlite3* db, const string& start_w, int max_len = 10) {
 
     for (int i = 0; i < max_len; ++i) {
         string next;
-        int max_weight = -1; //sql max num
         string sql = "SELECT neighbor, weight FROM memory WHERE word = ? ORDER BY weight DESC LIMIT 1;"; //getting first word with max weight with limit 1;
 
         sqlite3_stmt* tmp;
+        sqlite3_prepare_v2(db, sql.c_str(), -1, &tmp, nullptr);
         sqlite3_bind_text(tmp, 1, current.c_str(), -1, SQLITE_TRANSIENT); //bind curr word to ? in sql request
         if (sqlite3_step(tmp) == SQLITE_ROW) { //if we have what to read, read it and give me its naighbor
             const char* neighbor = (const char*)sqlite3_column_text(tmp, 0);
